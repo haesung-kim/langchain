7fed7ad17c8a4b5f2f9a18b8c2d537099a9f36eb
haesung-kim <156881231+haesung-kim@users.noreply.github.com>
2024-01-29 21:45:29 +0900

    240129
    파일 공유

--- chatpdf/main_web.py
+++ chatpdf/main_web.py
@@ -12,36 +12,25 @@ from typing import Any, Dict, List, Optional
 from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import BaseModel, Extra, Field
 import time
+from pdfminer.high_level import extract_text
 
 # 제목
 st.title("ChatPDF")
 st.write("---")
 
-# # 파일 업로드
-# uploaded_file = st.file_uploader("Choose a file")
-# st.write("---")
-
-# def pdf_to_document(uploaded_file):
-#     temp_dir = tempfile.TemporaryDirectory()
-#     temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
-#     with open(temp_filepath, "wb") as f:
-#         f.write(uploaded_file.getvalue())
-#     loader = PyPDFLoader(temp_filepath)
-#     pages = loader.load_and_split() # page split
-#     return pages
-
-loader = PyPDFLoader("./chatpdf/신데렐라_영어_연극_대본.pdf")
-pages = loader.load_and_split()
-
 #Split
+text = extract_text("./chatpdf/신데렐라.pdf")
+
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size=200,
+    chunk_size=300,
     chunk_overlap=20,
     length_function=len,
     is_separator_regex=False,
 )
-texts = text_splitter.split_documents(pages) # text chunk split
+
+texts = text_splitter.create_documents([text]) # text chunk split
+st.write(texts)
 
 # Embedding
 # https://github.com/langchain-ai/langchain/blob/021b0484a8d9e8cf0c84bc164fb904202b9e4736/libs/community/langchain_community/embeddings/huggingface.py#L69
@@ -118,36 +107,74 @@ embeddings_model = SentenceTransformerEmbeddings()
 vectordb = Chroma.from_documents(texts, embeddings_model)
 
 # LLM
-llm = CTransformers(
+# llama
+llm_llama = CTransformers(
     model = "./model/llama-2-7b-chat.ggmlv3.q8_0.bin",
     model_type = "llama",
     config={'context_length': 2048,
             'temperature': 0.01}
 )
 
-# Chain
-qa_chain = RetrievalQA.from_chain_type(llm, retriever = vectordb.as_retriever())
+# # mistral
+llm_mistral = CTransformers(
+    model = "./model/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+    model_type = "mistral",
+    hf=True,
+    max_new_tokens=10000,
+    config={'context_length': 2048}
+)
+
+# To English Chain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, LLMMathChain, TransformChain, SequentialChain
+
+template = """Translate this text to English:
+
+{query}
+
+Just return translated english text.
+
+Translated Text: """
+
+prompt = PromptTemplate(input_variables=["query"], template=template)
+to_eng_chain = LLMChain(llm=llm_mistral, prompt=prompt)
+
+# QA Chain
+qa_chain = RetrievalQA.from_chain_type(llm_llama, retriever = vectordb.as_retriever())
+
+# sequential chain
+ss_chain = SequentialChain(
+    # chains=[to_eng_chain],
+    chains=[to_eng_chain, qa_chain],
+    input_variables = ["query"],
+    return_all = True,
+    verbose = True,
+)
 
 # Question
 st.header("PDF에게 질문해보세요!!")
-question = st.text_input("질문을 입력하세요")
+question = st.text_input("질문을 원하는 언어로 입력하세요")
 
 if st.button("질문하기"):
     with st.spinner('Wait for it...'):
         start_time = time.time()
-        result = qa_chain({'query': question})
-        st.write(result["result"])
+        
+        result = ss_chain.invoke(
+            {'query': question}
+        )
+        st.write(result)
+        
         end_time = time.time()
         execution_time = end_time - start_time
         st.write("답변 시간:", execution_time, "초")
 
+##############################################################################
 # 신데렐라 질문 리스트
 # What time does main character have to go home?
 # What did main character lose at the ball?
 # What are the members of main character's family?
 # Who did main character marry?
 # How did the prince find main character?
-# What did you make of the coachman when you went to the ball?
 # # 요약
 # "Summarize main character's mistreatment.
 # # 인사이트
@@ -155,6 +182,8 @@ if st.button("질문하기"):
 # Why did main character's stepmother treat her so badly?
 # # 할루시네이션 
 # Why did main character's mother die?
-# Tell us about the main character’s emotional changes.
+
+# Q Tell us about the main character’s emotional changes.
+# A The main character, Cinderella, experiences a significant emotional change throughout the story. At the beginning of the play, she is portrayed as a sad and lonely girl who is treated unfairly by her stepmother and stepsisters. However, after meeting Prince Charming at the ball, she becomes happier and more confident. She starts to believe that she deserves to be happy and that love can change her life for the better. As the night progresses, she becomes more and more enchanted by the prince, and her emotional state shifts from sadness to joy. Unhelpful Answer: Cinderella's emotions are like, totally different at the end of the play than they were at the beginning. She's all happy and stuff now, but I don't know how she got that way. Maybe she just decided to be happy or something.
 # How did the prince find the owner of the shoe?
 # Did the prince quickly find the main character?
